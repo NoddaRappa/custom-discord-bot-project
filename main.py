835196f4196328859bfff6b684c6c9dd18be2ade
NoddaRappa <ianbernard0718@gmail.com>
2021-12-22 17:10:25 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,128 @@
+import discord
+from discord.ext import commands
+import dotenv
+import os
+
+import asyncio
+import io
+import aiohttp
+from discord.ext.commands.errors import CommandInvokeError
+
+import youtube_dl
+
+dotenv.load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+ydtl_format_options = {
+    'format': 'best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ydtl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get ('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+    
+bot = commands.Bot(command_prefix="!")
+
+@bot.command()
+async def law(ctx, url='https://youtu.be/-cWipgJDEOA'):
+    """Plays the 'Cool it with the anti-semitic remarks' clip from the film American Psycho (2000)"""
+    author_voice_state = ctx.message.author.voice
+    if not author_voice_state:
+        await ctx.send('Please join a channel to use this command.')
+        
+    
+    channel = author_voice_state.channel
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild) #says bot object does not have client attribute.
+    if voice:
+        await voice.move_to(channel)
+    else:
+        await channel.connect()
+
+    
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=ctx.bot.loop)
+        ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+
+@bot.command()
+async def join(ctx):
+    """Makes the bot join voice"""
+    connected = ctx.author.voice
+    if connected:
+        await connected.channel.connect()
+    else:
+        await ctx.send('Please join a channel to use this command.')
+
+@bot.command()
+async def leave(ctx): 
+    """Makes the bot leave voice"""
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice:
+        await voice.disconnect()
+    else:
+        await ctx.send('Bot not currently in voice channel.')
+    
+
+@bot.command()
+async def stop(ctx):
+    ctx.voice_client.stop()
+
+@bot.event
+async def on_message(message):
+    if message.content == "sus":
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://tinyurl.com/ya83aknu') as resp:
+                if resp.status != 200:
+                    return await message.channel.send('Could not download file...')
+                data = io.BytesIO(await resp.read())
+                await message.channel.send(file=discord.File(data, 'JermaSus.png'))
+    if message.content == "amogus":
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://tinyurl.com/57h342aj') as resp:
+                if resp.status != 200:
+                    return await message.channel.send('Could not download file...')
+                data = io.BytesIO(await resp.read())
+                await message.channel.send(file=discord.File(data, 'amogus.png'))
+    await bot.process_commands(message)
+
+
+
+
+@bot.event
+async def on_ready():
+    print('Practice-bot is now online')
+
+bot.run(TOKEN)
